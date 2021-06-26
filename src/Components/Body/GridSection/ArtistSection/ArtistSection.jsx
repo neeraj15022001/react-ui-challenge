9e5ab15b95be10e7e4b507862021601e2447f02f
neeraj15022001 <gneeraj32595@gmail.com>
2021-06-27 00:04:03 +0530

    added genres section

---
+++ src/Components/Body/GridSection/ArtistSection/ArtistSection.jsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import ArtistCard from "./ArtistCard/ArtistCard";
+import {
+  alan,
+  billie,
+  camila,
+  chainsmoker,
+  kanye,
+  martin,
+  nicki,
+  travis,
+} from "../Images.js";
+
+function ArtistSection() {
+    return (
+        <Container className="overflow-auto d-flex align-items-center justify-content-start">
+                <ArtistCard
+                  image={travis}
+                  bgColor="lightblue"
+                  artistName="travis scott"
+                  playCount="44"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={billie}
+                  bgColor="lightyellow"
+                  artistName="billie ellish"
+                  playCount="203"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={alan}
+                  bgColor="lightgray"
+                  artistName="alan walker"
+                  playCount="63"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={kanye}
+                  bgColor="lightcoral"
+                  artistName="kanye"
+                  playCount="15"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={nicki}
+                  bgColor="lightgrey"
+                  artistName="nicki minaj"
+                  playCount="180"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={camila}
+                  bgColor="lightcoral"
+                  artistName="camila cabello"
+                  playCount="180"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={chainsmoker}
+                  bgColor="lightgrey"
+                  artistName="chainsmokers"
+                  playCount="100"
+                  className="me-5"
+                />
+                <ArtistCard
+                  image={martin}
+                  bgColor="lightblue"
+                  artistName="martin garrix"
+                  playCount="150"
+                />
+              </Container>
+    )
+}
+
+export default ArtistSection
--- src/Components/Body/GridSection/GridSection.jsx
+++ src/Components/Body/GridSection/GridSection.jsx
@@ -1,16 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import ArtistCard from "./ArtistCard/ArtistCard";
-import {
-  alan,
-  billie,
-  camila,
-  chainsmoker,
-  kanye,
-  martin,
-  nicki,
-  travis,
-} from "./Images.js";
 import "./GridSection.css";
 import CardHeadline from "./CardHeadline/CardHeadline";
 import { MusicNoteList } from "react-bootstrap-icons";
@@ -27,63 +16,7 @@ function GridSection() {
               fluid
             >
               <CardHeadline title="top artists" secondaryTitle="see all" />
-              <Container className="overflow-auto d-flex align-items-center justify-content-start">
-                <ArtistCard
-                  image={travis}
-                  bgColor="lightblue"
-                  artistName="travis scott"
-                  playCount="44"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={billie}
-                  bgColor="lightyellow"
-                  artistName="billie ellish"
-                  playCount="203"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={alan}
-                  bgColor="lightgray"
-                  artistName="alan walker"
-                  playCount="63"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={kanye}
-                  bgColor="lightcoral"
-                  artistName="kanye"
-                  playCount="15"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={nicki}
-                  bgColor="lightgrey"
-                  artistName="nicki minaj"
-                  playCount="180"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={camila}
-                  bgColor="lightcoral"
-                  artistName="camila cabello"
-                  playCount="180"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={chainsmoker}
-                  bgColor="lightgrey"
-                  artistName="chainsmokers"
-                  playCount="100"
-                  className="me-5"
-                />
-                <ArtistCard
-                  image={martin}
-                  bgColor="lightblue"
-                  artistName="martin garrix"
-                  playCount="150"
-                />
-              </Container>
+              
             </Container>
           </Container>
           <Container className="h-50" fluid>
