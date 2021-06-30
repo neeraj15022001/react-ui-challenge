160d78560eac64fbfcd5fa4fcbbca4481881b526
neeraj15022001 <gneeraj32595@gmail.com>
2021-06-30 20:48:45 +0530

    fixed genre card

--- src/Components/Body/GridSection/GenreCard/GenreCard.jsx
+++ src/Components/Body/GridSection/GenreCard/GenreCard.jsx
@@ -6,9 +6,9 @@ function GenreCard({line1,line2, bgColor, className}) {
         borderRadius : "1rem"
     }
     return (
-        <div className={`${className} d-flex align-items-start justify-content-center flex-column h-100`} style={genreCard}>
-            <p className="text-capitalize fw-bold fs-5 w-100 ms-4">{line1}</p>
-            <p className="text-capitalize fw-bold fs-5 w-100 ms-4">{line2}</p>
+        <div className={`${className} d-flex align-items-start justify-content-center flex-column h-100 py-3`} style={genreCard}>
+            <p className="text-capitalize fw-bold fs-6 w-100 ms-4">{line1}</p>
+            <p className="text-capitalize fw-bold fs-6 w-100 ms-4">{line2}</p>
         </div>
     )
 }
